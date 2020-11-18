76cb923ff00fe9bd46c5014a3e4469e3d09777ce
theonlywisdom <mailbaah@gmail.com>
2020-11-18 14:58:00 +0000

    Finished both challenges

--- src/CardGame/IPackOfCards.cs
+++ src/CardGame/IPackOfCards.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace CodingChallenge.CardGame
 {
@@ -8,4 +11,5 @@ namespace CodingChallenge.CardGame
 
         ICard TakeCardFromTopOfPack();
     }
+
 }
--- src/ReversingString/StringUtilities.cs
+++ src/ReversingString/StringUtilities.cs
@@ -4,9 +4,25 @@ namespace CodingChallenge.ReversingString
 {
     public class StringUtilities
     {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+            string s = Console.ReadLine();
+            Console.Write("Output: ");
+            Console.WriteLine(Reverse(s));
+        }
+
         public static string Reverse(string s)
         {
-            throw new NotImplementedException();
+            // optimises for null, empty, and single character inputs
+            if (string.IsNullOrEmpty(s) || s.Length == 1)
+            {
+                return s;
+            }
+
+            char[] stringChars = s.ToCharArray();
+            Array.Reverse(stringChars);
+            return new string(stringChars);
         }
     }
 }
